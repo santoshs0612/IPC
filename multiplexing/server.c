17c672843e9e8e52398d8b36c58bcc68ddf9bf37
santoshs0612 <santoshs0612@gmail.com>
2023-06-13 07:08:28 +0530

    Multi client server fd data structure added

---
+++ multiplexing/server.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  server.c
+ *
+ *    Description: server multiplexing  
+ *
+ *        Version:  1.0
+ *        Created:  12/06/23 08:44:43 PM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Santosh Kumar, 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
